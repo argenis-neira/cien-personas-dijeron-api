b298c0823736bb051008552dd0ff1eeb0d9f265c
madfourier <cneira@londonuw.com>
2024-06-23 01:12:41 -0500

    conection mongo

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import json
-import time
 
 app = Flask(__name__)
 CORS(app)
+
+uri = "mongodb+srv://neiracarlosargenis:WSGc5d39D9dJuSV7@cluster0.wr2s1ph.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+routeFiles = "."
+# routeFiles = "/home/argenisneira/mysite"
 
 @app.route('/')
 def root():
@@ -13,7 +29,7 @@ def root():
 @app.route('/save', methods=['POST'])
 def save_data():
   data = request.get_json()
-  with open("datos.txt", 'w') as archivo:
+  with open(routeFiles + "/datos.txt", 'w') as archivo:
     # Escribir el contenido en el archivo
     archivo.write(json.dumps(data))
   return jsonify(data)
@@ -23,7 +39,7 @@ def get_data():
   #algo que se puede mejorar es desde el front enviar un objeto con 3 keys
   #pero lo puedo manejar en el backend para que sean leidos por la otra pagina
   result = []
-  with open("datos.txt", 'r') as archivo:
+  with open(routeFiles + "/datos.txt", 'r') as archivo:
     contenido = archivo.read()
     dataJson = json.loads(contenido)
     element_count = len(dataJson) // 3
@@ -34,47 +50,66 @@ def get_data():
         "show" : dataJson[f"show{i}"],
       })
   #the one who first buzz
-  with open('buzz.txt', 'r') as archivo:
+  with open(routeFiles + '/buzz.txt', 'r') as archivo:
     first_line = archivo.readline().strip()
     first_buz = first_line if first_line else '{"usuario": ""}'
-  
-  #if the button wrong is pressed
-  with open('wrong.txt', 'r') as archivo:
-    first_line = archivo.readline().strip()
-    is_it_wrong = True if first_line == 'True' else False
 
-  return jsonify({"values": result, "first" : first_buz, "wrong" : is_it_wrong})
+  #if the button wrong is pressed
+  with open(routeFiles + '/wrong.txt', 'r') as archivo:
+    wrong = archivo.readline().strip()
+
+  return jsonify({"values": result, "first" : first_buz, "wrong" : wrong})
 
 #handle buzz
 @app.route('/buzz', methods=['POST'])
 def first_buz():
   data = request.get_json()
-  with open('buzz.txt', 'a') as archivo:
+  with open(routeFiles + '/buzz.txt', 'a') as archivo:
     archivo.write(json.dumps(data) + "\n")
   return "OK"
 
 @app.route('/reset_buzz', methods=['GET'])
 def reset_buzz():
-  with open('buzz.txt', 'w') as archivo:
+  with open(routeFiles + '/buzz.txt', 'w') as archivo:
     archivo.write('')
   return "OK"
 
 #show wrong for a few seconds
-@app.route('/wrong', methods=['GET'])
+@app.route('/wrong', methods=['POST'])
 def show_wrong():
-  with open('wrong.txt', 'w') as archivo:
-    archivo.write('True')
-  time.sleep(4)
-  with open('wrong.txt', 'w') as archivo:
-    archivo.write('False')
+  data = request.get_json()
+  number = data['wrong']
+  with open(routeFiles + '/wrong.txt', 'w') as archivo:
+    archivo.write(str(number))
   return "OK"
 
-# @app.route('/get_first_buzz', methods=['GET'])
-# def get_first():
-#   with open('buzz.txt', 'r') as archivo:
-#     first_line = archivo.readline().strip
-#     first_buz = first_line if first_line else '{"usuario": ""}'
-#   return jsonify(first_buz)
+#Logica que incluye conectarse a la base de datos
+@app.route("/get_games",methods=['GET'])
+def get_games():
+  t=client.cien_personas_dijeron.matches.find()
+  # Convertir cursor a una lista de diccionarios  
+  documents = []
+  for document in t:
+      # Convierte ObjectId a str para evitar el error de serialización
+      document['_id'] = str(document['_id'])
+      documents.append(document)
+  return jsonify(documents), 200
+
+#SEGUNDA APPLICACION
+@app.route('/save_canvas', methods=['POST'])
+def save_canvas():
+  data = request.get_json()
+  with open("/home/argenisneira/canvas-form-api/database.txt", 'a') as archivo:
+    archivo.write(json.dumps(data) + "\n")
+  return "OK"
+
+@app.route('/get_canvas', methods=['GET'])
+def get_canvas():
+  data = []
+  with open("/home/argenisneira/canvas-form-api/database.txt", "r") as archivo:
+    for linea in archivo:
+      data.append(json.loads(linea))
+  return jsonify({"array": data})
 
 if __name__ == '__main__':
   app.run(debug=False)
